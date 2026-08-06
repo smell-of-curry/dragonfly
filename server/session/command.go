5c495c773fbdfc07e2d40178500b8c828aca9ee0
smell-of-curry <75345244+smell-of-curry@users.noreply.github.com>
2026-08-06 13:48:05 -0400

    session: adapt to gophertunnel master API breaks
    CommandArgTypeValue removed (use RValue wire id 4); AttachedToEntityID is Optional[int64].

--- server/session/command.go
+++ server/session/command.go
@@ -213,7 +213,8 @@ func valueToParamType(i cmd.ParamInfo, source cmd.Source) (t uint32, enum comman
 			Options: enum.Options(source),
 		}
 	}
-	return protocol.CommandArgTypeValue, enum
+	// CommandArgTypeValue was removed in gophertunnel; wire ID 4 is now CommandArgTypeRValue.
+	return protocol.CommandArgTypeRValue, enum
 }
 
 // resendCommands resends all commands that a Session has access to if the map of runnable commands passed does not
